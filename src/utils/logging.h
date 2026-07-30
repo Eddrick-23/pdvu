639876fff8c54528e18e348bd8fd347f624c1a69
Eddrick <eddricklivando@gmail.com>
2026-07-31 01:17:14 +0800

    feat: make logging configurable via cli flag instead of a build option revamps logging setup to use an initialiser that defaults to none if logging is not enabled if --log flag is passed in and loggin setup fails, we exit immediately.

--- src/utils/logging.h
+++ src/utils/logging.h
@@ -1,27 +1,36 @@
 #pragma once
+#include <expected>
+#include <filesystem>
+#include <string>
 
-struct NullStream {
-  template <typename T>
-  NullStream& operator<<(const T&) {
-    return *this;
+#include "plog/Initializers/RollingFileInitializer.h"
+
+namespace Logging {
+
+/**
+ *  @breif sets up plog logger to write to target log_file
+ * @param enable_logging Control if plog should be set up or not.
+ *                       If false defaults to plog::none which drops all logs
+ * @param log_file path to log file
+ * @return true if setup successful, false otherwise.
+ */
+[[nodiscard]] inline std::expected<void, std::string> setup_logging(
+    bool enable_logging, const std::filesystem::path& log_file) {
+  if (!enable_logging) {
+    plog::init(plog::none, log_file.c_str());
+    return {};
   }
-};
 
-#ifdef ENABLE_LOGGING
-#include <plog/Initializers/RollingFileInitializer.h>
-#include <plog/Log.h>
-#else
-// Disable plog logging macros
-#define PLOG_VERBOSE \
-  if (false) NullStream()
-#define PLOG_DEBUG \
-  if (false) NullStream()
-#define PLOG_INFO \
-  if (false) NullStream()
-#define PLOG_WARNING \
-  if (false) NullStream()
-#define PLOG_ERROR \
-  if (false) NullStream()
-#define PLOG_FATAL \
-  if (false) NullStream()
-#endif
+  try {
+    // manually ensure parent path exists, plog won't auto create paths
+    if (log_file.has_parent_path()) {
+      std::filesystem::create_directories(log_file.parent_path());
+    }
+    // Initialise Plog
+    plog::init(plog::debug, log_file.c_str(), 10'000'000, 1);
+    return {};
+  } catch (const std::exception& e) {
+    return std::unexpected(std::string(e.what()));
+  }
+}
+}  // namespace Logging
